234a9edfb25ae30f543767a99f5fa273fc397ff4
DhiegoKricowski <dhiegokricowski917@gmail.com>
2025-07-28 09:48:15 -0300

    oi

---
+++ aula0.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")();
+
+let nome = prompt("digite o seu nome :");
+let idade = prompt("digite a sua idade :");
+let altura = prompt("digite a sua altura :");
+let cpf = prompt("gitite seu cpf :");
+
+
+console.log("Bem vindo", nome)
+console.log("voce tem", idade, "Anos de idade");
+console.log(altura, "de altura");
+console.log("seu cpf é", cpf,);
